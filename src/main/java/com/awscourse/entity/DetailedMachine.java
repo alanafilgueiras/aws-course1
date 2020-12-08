d4b7a16a09ea7f38b1dd6db7d04bf68c4b9db961
alanafilgueiras <75276526+alanafilgueiras@users.noreply.github.com>
2020-12-08 11:38:04 -0300

    Update DetailedMachine.java

--- src/main/java/com/awscourse/entity/DetailedMachine.java
+++ src/main/java/com/awscourse/entity/DetailedMachine.java
@@ -1,9 +1,23 @@
 package com.awscourse.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter @Setter
 public class DetailedMachine extends Machine {
 	private String javaVersion;
 	private String javaVendor;
 	private String javaVendorURL;
+	
+	public DetailedMachine(String ip, String name, String os, String architecture, String osVersion, String javaVersion,
+			String javaVendor, String javaVendorURL) {
+		super(ip, name, os, architecture, osVersion);
+		this.javaVersion = javaVersion;
+		this.javaVendor = javaVendor;
+		this.javaVendorURL = javaVendorURL;
+	}
 	
 
 }
