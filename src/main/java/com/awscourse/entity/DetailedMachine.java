e9f3d8a12e5ca31765650ca47e9f60e8ab3f477e
alanafilgueiras <75276526+alanafilgueiras@users.noreply.github.com>
2020-12-08 11:30:46 -0300

    Create entities

---
+++ src/main/java/com/awscourse/entity/DetailedMachine.java
@@ -0,0 +1,9 @@
+package com.awscourse.entity;
+
+public class DetailedMachine extends Machine {
+	private String javaVersion;
+	private String javaVendor;
+	private String javaVendorURL;
+	
+
+}
---
+++ src/main/java/com/awscourse/entity/Machine.java
@@ -0,0 +1,19 @@
+package com.awscourse.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Machine {
+	private String ip;
+	private String name;
+	private String os;
+	private String architecture;
+	private String osVersion;
+	
+	
+	
+}
